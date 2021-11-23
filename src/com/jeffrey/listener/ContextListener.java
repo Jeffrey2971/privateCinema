78d8497720c7b4c322381666698808a5ce8a5aba
Jeffrey <Jeffrey2971@outlook.com>
2021-11-23 22:01:36 +0800

    cinema：使用多线程异步执行初始化操作，在导出缩略图操作时可观看视频

--- src/com/jeffrey/listener/ContextListener.java
+++ src/com/jeffrey/listener/ContextListener.java
@@ -17,48 +17,46 @@ public class ContextListener implements ServletContextListener {
     private static final ThumbnailService THUMBNAIL_SERVICE = new ThumbnailServiceImpl();
     private static final ArrayList<File> LISTS = new ArrayList<>();
 
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        new Thread(() -> {
+            // 对表数据进行充重置
+            THUMBNAIL_SERVICE.truncateData();
 
+            ServletContext servletContext = sce.getServletContext();
 
+            // 监听目录
+            File target = new File(servletContext.getRealPath("/static/video"));
 
+            // 查找监听目录内的所有 mp4 视频
+            findVideo(target);
 
-        // 对表数据进行充重置
-        THUMBNAIL_SERVICE.truncateData();
+            String thumbnailPath = servletContext.getRealPath("/static/thumbnail/");
 
-        ServletContext servletContext = sce.getServletContext();
-
-        // 监听目录
-        File target = new File(servletContext.getRealPath("/static/video"));
-
-        // 查找监听目录内的所有 mp4 视频
-        findVideo(target);
-
-        String thumbnailPath = servletContext.getRealPath("/static/thumbnail/");
-
-        File[] files = new File(thumbnailPath).listFiles();
-        if (files != null) {
-            for (File item : files) {
-                if (item.delete()) {
-                    System.out.println("移除文件：" + item);
+            File[] files = new File(thumbnailPath).listFiles();
+            if (files != null) {
+                for (File item : files) {
+                    if (item.delete()) {
+                        System.out.println("移除文件：" + item);
+                    }
                 }
             }
-        }
 
-        ThumbnailDataManagement.createThumbnail(LISTS, thumbnailPath);
+            ThumbnailDataManagement.createThumbnail(LISTS, thumbnailPath);
 
-        FileAlterationObserver observer = new FileAlterationObserver(target);
+            FileAlterationObserver observer = new FileAlterationObserver(target);
 
-        observer.addListener(new MonitorVideoModify(thumbnailPath, servletContext.getRealPath("/")));
+            observer.addListener(new MonitorVideoModify(thumbnailPath, servletContext.getRealPath("/")));
 
-        FileAlterationMonitor monitor = new FileAlterationMonitor(5000, observer);
+            FileAlterationMonitor monitor = new FileAlterationMonitor(5000, observer);
 
-        try {
-            monitor.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+            try {
+                monitor.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     @Override
